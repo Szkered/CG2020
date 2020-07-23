a28395f74583199390fb76c030b04f33a133ba6c
Zekun Shi <zekun@neuri.ai>
2020-07-23 18:02:10 +0800

    clang-format

--- CCGHomework_skeleton/3rdparty/MeshLib/core/Parser/parser.h
+++ CCGHomework_skeleton/3rdparty/MeshLib/core/Parser/parser.h
@@ -1,10 +1,10 @@
 /*!
-*      \file parser.h
-*      \brief Parse the trait strings of vertex, edge, halfedge , face etc.
-*	   \author David Gu
-*      \date 10/07/2010
-*
-*/
+ *      \file parser.h
+ *      \brief Parse the trait strings of vertex, edge, halfedge , face etc.
+ *	   \author David Gu
+ *      \date 10/07/2010
+ *
+ */
 
 #ifndef _MESHLIB_PARSER_H_
 #define _MESHLIB_PARSER_H_
@@ -19,212 +19,216 @@ namespace MeshLib
 
 /*!
  *	\brief CToken  class, key=(value), e.g. uv=(x y)
-*/
+ */
 
 class CToken
 {
-public:
-	/*! key of the token */
-	std::string m_key;
-	/*! value of the token */
-	std::string m_value;
+  public:
+    /*! key of the token */
+    std::string m_key;
+    /*! value of the token */
+    std::string m_value;
 };
 
 /*!
  *	\brief CParser class
-*/
+ */
 
 class CParser
 {
-public:
-	/*!
-	 *	\brief CParser constructor
-	 *  \param str input string
-	 */
-	CParser( const std::string & str)
-	{
-		//copy string
-		unsigned int i;
-		for(  i = 0; i < str.length(); i ++ )
-		{
-			m_line[i] = str.c_str()[i];
-		}
-		m_line[i] = 0;
+  public:
+    /*!
+     *	\brief CParser constructor
+     *  \param str input string
+     */
+    CParser(const std::string &str)
+    {
+        // copy string
+        unsigned int i;
+        for (i = 0; i < str.length(); i++)
+        {
+            m_line[i] = str.c_str()[i];
+        }
+        m_line[i] = 0;
 
-		m_pt = m_line;
-		
-		//while not reaching the end
-		while( !end() )
-		{
-			skip_blank();
-			if( end() ) break;
+        m_pt = m_line;
 
-			//copy key
+        // while not reaching the end
+        while (!end())
+        {
+            skip_blank();
+            if (end())
+                break;
 
-			char * pkey = m_key;
-			char   ch = next_char();
+            // copy key
 
-			while( ch != ' ' && ch != '=' && !end() )
-			{
-				*pkey ++ = ch;
-				ch = next_char();
-			}
-			if( ch != '=' && ch != ' ')
-				*pkey++ = ch;
+            char *pkey = m_key;
+            char ch = next_char();
 
-			*pkey = 0;
+            while (ch != ' ' && ch != '=' && !end())
+            {
+                *pkey++ = ch;
+                ch = next_char();
+            }
+            if (ch != '=' && ch != ' ')
+                *pkey++ = ch;
 
-			if( ch == ' ' )
-			{
-				skip_blank();
-			}
+            *pkey = 0;
 
-			if( ch != '=' )
-			{
-				CToken *	tk = new CToken;
-				assert(tk);
-				tk->m_key  = std::string( m_key );
-				
-				m_tokens.push_back( tk );
-				continue;
-			}
+            if (ch == ' ')
+            {
+                skip_blank();
+            }
 
-			if( end() ) break;
+            if (ch != '=')
+            {
+                CToken *tk = new CToken;
+                assert(tk);
+                tk->m_key = std::string(m_key);
 
-			ch = next_char();
+                m_tokens.push_back(tk);
+                continue;
+            }
 
-			while( ch != '(' ) ch = next_char();
+            if (end())
+                break;
 
-			char * pvalue = m_value;
+            ch = next_char();
 
-			while( ch != ')' && !end() )
-			{
-				*pvalue ++ = ch;
-				ch = next_char();
-			}
-			*pvalue++ = ch;
-			*pvalue  = 0;
+            while (ch != '(')
+                ch = next_char();
 
-			CToken *	tk = new CToken;
-			assert(tk);
-			tk->m_key   = std::string( m_key   );
-			tk->m_value = std::string( m_value );
-			
-			m_tokens.push_back( tk );
-			
-		}
+            char *pvalue = m_value;
 
-	};
+            while (ch != ')' && !end())
+            {
+                *pvalue++ = ch;
+                ch = next_char();
+            }
+            *pvalue++ = ch;
+            *pvalue = 0;
 
-	/*!
-	 *	CParser Destructor
-	 */
-	~CParser()
-	{
-	  //release all the tokens
+            CToken *tk = new CToken;
+            assert(tk);
+            tk->m_key = std::string(m_key);
+            tk->m_value = std::string(m_value);
 
-	  for( std::list<CToken*>::iterator titer = m_tokens.begin(); titer != m_tokens.end(); titer ++ )
-	  {
-		  CToken * pT = *titer;
-		  delete pT;
-	  }
-	  m_tokens.clear();
-	};
+            m_tokens.push_back(tk);
+        }
+    };
 
-	/*!
-	 *	List of tokens extracted from the string
-	 */
-	std::list<CToken*> & tokens() { return m_tokens; };
-	
-	/*!
-	 *	Convert the list of tokens to a string
-	 *  \param str the output string
-	 */
-	void _toString( std::string & str )
-	{
-		std::stringstream iss;
+    /*!
+     *	CParser Destructor
+     */
+    ~CParser()
+    {
+        // release all the tokens
 
-		for( std::list<CToken*>::iterator iter = m_tokens.begin() ; iter != m_tokens.end(); ++ iter )
-		{
-			  CToken * token = *iter;
-			  if( iss.str().length() > 0 ) iss << " ";
-			  if( token->m_value.empty() )
-			  {
-				iss << token->m_key;
-			  }
-			  else
-			  {
-				iss << token->m_key << "=" << token->m_value;			  
-			  }
-		}
+        for (std::list<CToken *>::iterator titer = m_tokens.begin(); titer != m_tokens.end(); titer++)
+        {
+            CToken *pT = *titer;
+            delete pT;
+        }
+        m_tokens.clear();
+    };
 
-		str = iss.str();
-	};
-	/*!
-	 *	Remove the token key=(...) from the current string
-	 *  \param key the key to the token to be removed
-	 */
-	void _removeToken( const std::string & key )	
-	{
-		for( std::list<CToken*>::iterator iter = m_tokens.begin() ; iter != m_tokens.end(); ++ iter )
-		{
-			  CToken * token = *iter;
-			  if( token->m_key == key )
-			  {
-				  m_tokens.erase( iter );
-				  return;
-			  }
-		}
-	};
+    /*!
+     *	List of tokens extracted from the string
+     */
+    std::list<CToken *> &tokens()
+    {
+        return m_tokens;
+    };
 
-private:
+    /*!
+     *	Convert the list of tokens to a string
+     *  \param str the output string
+     */
+    void _toString(std::string &str)
+    {
+        std::stringstream iss;
 
-	/*!
-	 *	get the next char in the current string
-	 */
-	char next_char()
-	{
-		char ch= *m_pt;
-		m_pt++;
-		return ch;
-	};
-	/*!
-	 *	skip blank spaces
-	 */
-	void skip_blank()
-	{
-		while( *m_pt == ' ' ) m_pt++;
-	};
-	/*!
-	 *	verify if we 
-	 */
-	bool end()
-	{
-		return ( (*m_pt) == 0 );
-	};
-	/*!
-	 *	list of tokens
-	 */
-	std::list<CToken*> m_tokens;
-	
-	/*!
-	 * The buffer to contain the string
-	 */
-	char m_line[1024];
-	/*!
-	 *	current key
-	 */
-	char m_key[1024];
-	/*!
-	 *	current value
-	 */
-	char m_value[1024];
-	/*!
-	 *	current pointer to the char buffer
-	 */
-	char * m_pt;
+        for (std::list<CToken *>::iterator iter = m_tokens.begin(); iter != m_tokens.end(); ++iter)
+        {
+            CToken *token = *iter;
+            if (iss.str().length() > 0)
+                iss << " ";
+            if (token->m_value.empty())
+            {
+                iss << token->m_key;
+            }
+            else
+            {
+                iss << token->m_key << "=" << token->m_value;
+            }
+        }
+
+        str = iss.str();
+    };
+    /*!
+     *	Remove the token key=(...) from the current string
+     *  \param key the key to the token to be removed
+     */
+    void _removeToken(const std::string &key)
+    {
+        for (std::list<CToken *>::iterator iter = m_tokens.begin(); iter != m_tokens.end(); ++iter)
+        {
+            CToken *token = *iter;
+            if (token->m_key == key)
+            {
+                m_tokens.erase(iter);
+                return;
+            }
+        }
+    };
+
+  private:
+    /*!
+     *	get the next char in the current string
+     */
+    char next_char()
+    {
+        char ch = *m_pt;
+        m_pt++;
+        return ch;
+    };
+    /*!
+     *	skip blank spaces
+     */
+    void skip_blank()
+    {
+        while (*m_pt == ' ')
+            m_pt++;
+    };
+    /*!
+     *	verify if we
+     */
+    bool end()
+    {
+        return ((*m_pt) == 0);
+    };
+    /*!
+     *	list of tokens
+     */
+    std::list<CToken *> m_tokens;
+
+    /*!
+     * The buffer to contain the string
+     */
+    char m_line[1024];
+    /*!
+     *	current key
+     */
+    char m_key[1024];
+    /*!
+     *	current value
+     */
+    char m_value[1024];
+    /*!
+     *	current pointer to the char buffer
+     */
+    char *m_pt;
 };
 
-
-}; //namespace 
+}; // namespace MeshLib
 #endif
--- CCGHomework_skeleton/3rdparty/MeshLib/core/viewer/Arcball.h
+++ CCGHomework_skeleton/3rdparty/MeshLib/core/viewer/Arcball.h
@@ -13,7 +13,8 @@
 #include "../Geometry/Point2.h"
 #include "../Geometry/quat.h"
 
-namespace MeshLib {
+namespace MeshLib
+{
 /*!
  *	\brief CArcball Interface
  *
@@ -24,82 +25,87 @@ namespace MeshLib {
  *  to rotate it. Everything inisde the ball with be roated
  *  accordingly.
  */
-class CArcball {
-public:
-  /*!
-   *	CArcball default constructor
-   */
-  CArcball(){};
+class CArcball
+{
+  public:
+    /*!
+     *	CArcball default constructor
+     */
+    CArcball(){};
 
-  /*!
-   *	CArcball constructor
-   *  \param width  width of the window
-   *  \param height height of the window
-   *  \param ox     original x position
-   *  \param oy     original y position
-   */
-  CArcball(int win_width, int win_height, int ox, int oy) {
-    m_radius = (win_width < win_height) ? win_width / 2 : win_height / 2;
+    /*!
+     *	CArcball constructor
+     *  \param width  width of the window
+     *  \param height height of the window
+     *  \param ox     original x position
+     *  \param oy     original y position
+     */
+    CArcball(int win_width, int win_height, int ox, int oy)
+    {
+        m_radius = (win_width < win_height) ? win_width / 2 : win_height / 2;
 
-    m_center = CPoint2(win_width / 2, win_height / 2);
+        m_center = CPoint2(win_width / 2, win_height / 2);
 
-    CPoint2 p(ox, oy);
+        CPoint2 p(ox, oy);
 
-    _plane2sphere(p, m_position);
-  };
+        _plane2sphere(p, m_position);
+    };
 
-  /*!
-   *	Update the arcball
-   *	\param nx current x-position of mouse
-   *    \param ny current y-position of mouse
-   *    \return quoternion of the roation from the old position to the new
-   *position
-   */
-  CQrot update(int nx, int ny) {
-    CPoint position;
-    _plane2sphere(CPoint2(nx, ny), position);
-    CPoint cp = m_position ^ position;
-    CQrot r(m_position * position, cp[0], cp[1], cp[2]);
-    m_position = position;
+    /*!
+     *	Update the arcball
+     *	\param nx current x-position of mouse
+     *    \param ny current y-position of mouse
+     *    \return quoternion of the roation from the old position to the new
+     *position
+     */
+    CQrot update(int nx, int ny)
+    {
+        CPoint position;
+        _plane2sphere(CPoint2(nx, ny), position);
+        CPoint cp = m_position ^ position;
+        CQrot r(m_position * position, cp[0], cp[1], cp[2]);
+        m_position = position;
 
-    return r;
-  };
+        return r;
+    };
 
-private:
-  /*!
-   *	mapping a planar point v to the unit sphere point r
-   *  \param v input planar point
-   *  \param r output point on the unit sphere
-   */
-  void _plane2sphere(const CPoint2 &v, CPoint &r) {
+  private:
+    /*!
+     *	mapping a planar point v to the unit sphere point r
+     *  \param v input planar point
+     *  \param r output point on the unit sphere
+     */
+    void _plane2sphere(const CPoint2 &v, CPoint &r)
+    {
 
-    CPoint2 f = v;
-    f /= m_radius;
+        CPoint2 f = v;
+        f /= m_radius;
 
-    double l = sqrt(f * f);
+        double l = sqrt(f * f);
 
-    if (l > 1.0) {
-      r = CPoint(f[0] / l, f[1] / l, 0);
-      return;
-    }
+        if (l > 1.0)
+        {
+            r = CPoint(f[0] / l, f[1] / l, 0);
+            return;
+        }
 
-    double fz = sqrt(1 - l * l);
+        double fz = sqrt(1 - l * l);
 
-    r = CPoint(f[0], f[1], fz);
-  };
+        r = CPoint(f[0], f[1], fz);
+    };
 
-  /*!
-   *	current position on the unit sphere
-   */
-  CPoint m_position;
-  /*!
-   *	radius of the sphere
-   */
-  double m_radius;
-  /*!
-   *	center of sphere on the plane
-   */
-  CPoint2 m_center;
+    /*!
+     *	current position on the unit sphere
+     */
+    CPoint m_position;
+    /*!
+     *	radius of the sphere
+     */
+    double m_radius;
+    /*!
+     *	center of sphere on the plane
+     */
+    CPoint2 m_center;
 };
 
 } // namespace MeshLib
